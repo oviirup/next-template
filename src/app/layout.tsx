9e98faee5bc476bb242e19e315b80936a212a605
oviirup <49820575+oviirup@users.noreply.github.com>
2026-02-08 22:37:57 +0530

    ♻️ updated style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
 import { Providers } from "@/components/layout/providers";
-import { fontClassNames } from "@/lib/fonts";
+import { fonts } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 
 export { metadata, viewport } from "./meta";
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
+  const fontClassNames = [fonts.sans.variable, fonts.code.variable];
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn(fontClassNames)}>
+      <body className={cn(fontClassNames, "flex flex-col bg-background px-2")}>
         <Providers>{children}</Providers>
       </body>
     </html>
