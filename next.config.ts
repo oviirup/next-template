e70a2332dbcc0a4b7d071e77c403ac200461b60d
oviirup <49820575+oviirup@users.noreply.github.com>
2026-07-23 20:01:38 +0530

    🔧 updated config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,23 @@
 import { NextConfig } from "next";
 
-const config: NextConfig = {
-  reactCompiler: true,
-  devIndicators: false,
-  cacheComponents: true,
-  typescript: { ignoreBuildErrors: true },
-};
-
-export default config;
+export default async function asyncConfig() {
+  return {
+    reactCompiler: true,
+    typescript: { ignoreBuildErrors: true }, // ignore typechecking during build
+    devIndicators: false, // hides dev indicator
+    allowedDevOrigins: ["192.168.1.*"],
+    headers: () => [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+          { key: "Cross-Origin-Resource-Policy", value: "same-origin" },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Creator", value: "oviirup" },
+        ],
+      },
+    ],
+  } satisfies NextConfig;
+}
